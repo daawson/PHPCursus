d5113e5c919932ee67db3107c1dbf9b95a4834cd
Dawid K <dkujawskiii@gmail.com>
2018-12-05 10:32:00 +0100

    ..

---
+++ Deel_01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 'Ik heb niet gefaald';
+$b = 'die niet werken';
+$c = 10000;
+
+echo $a . '. Ik heb alleen '.$c." maniere gevonden ".$b."<br> - <i>Thomas Edison</i><br>";
+
+
+$a = 'die geen fout';
+$b = 'en';
+$c = 'niet.';
+
+echo "Mens".$b." ".$a." mak".$b.", werk".$b." ".$c;
+echo "<br> - <i>Albert Piesman(KLM)</i><br>";
